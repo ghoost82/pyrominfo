64b607cb8750097a310b6ad7f43271785fc7f258
Jan Sperling <sperling@b1-systems.de>
2019-11-22 06:14:23 +0100

    DISC: decode date codes

--- pyrominfo/genericdisc.py
+++ pyrominfo/genericdisc.py
@@ -45,22 +45,45 @@ class GenericDiscParser(RomInfoParser):
     def parseBuffer(self, data):
         props = {}
         props["platform"] = "Generic Disc"
-        props["standard_id"] =  self._sanitize(data[0x8001 : 0x8001 + 5])
-        props["system_id"] =  self._sanitize(data[0x8008 : 0x8008 + 32])
-        props["volume_id"] =  self._sanitize(data[0x8028 : 0x8028 + 32])
-        props["set_info"] =  "%d/%d" % (data[0x807f], data[0x807b])
-        props["volume_set_id"] =  self._sanitize(data[0x80be : 0x80be + 128])
-        props["publisher_id"] =  self._sanitize(data[0x813e : 0x813e + 128])
-        props["data_preparer_id"] =  self._sanitize(data[0x81be : 0x81be + 128])
-        props["application_id"] =  self._sanitize(data[0x823e : 0x823e + 128])
-        props["copyright_file_id"] =  self._sanitize(data[0x82be : 0x82be + 37])
-        props["abstract_file_id"] =  self._sanitize(data[0x82e3 : 0x82e3 + 37])
-        props["bibliographic_file_id"] =  self._sanitize(data[0x8308 : 0x8308 + 37])
-        props["creation_date_code"] =  self._sanitize(data[0x832d : 0x832d + 17])
-        props["modification_date_code"] =  self._sanitize(data[0x833e : 0x833e + 17])
-        props["expiration_date_code"] =  self._sanitize(data[0x834f : 0x834f + 17])
-        props["effective_date_code"] =  self._sanitize(data[0x8360 : 0x8360 + 17])
+        props["standard_id"] = self._sanitize(data[0x8001 : 0x8001 + 5])
+        props["system_id"] = self._sanitize(data[0x8008 : 0x8008 + 32])
+        props["volume_id"] = self._sanitize(data[0x8028 : 0x8028 + 32])
+        props["set_info"] = "%d/%d" % (data[0x807f], data[0x807b])
+        props["volume_set_id"] = self._sanitize(data[0x80be : 0x80be + 128])
+        props["publisher_id"] = self._sanitize(data[0x813e : 0x813e + 128])
+        props["data_preparer_id"] = self._sanitize(data[0x81be : 0x81be + 128])
+        props["application_id"] = self._sanitize(data[0x823e : 0x823e + 128])
+        props["copyright_file_id"] = self._sanitize(data[0x82be : 0x82be + 37])
+        props["abstract_file_id"] = self._sanitize(data[0x82e3 : 0x82e3 + 37])
+        props["bibliographic_file_id"] = self._sanitize(data[0x8308 : 0x8308 + 37])
+        #props["creation_date_code"] = self._sanitize(data[0x832d : 0x832d + 17])
+        props["creation_date"] = self._convertPvgDate(data[0x832d : 0x832d + 17])
+        #props["modification_date_code"] = self._sanitize(data[0x833e : 0x833e + 17])
+        props["modification_date"] = self._convertPvgDate(data[0x833e : 0x833e + 17])
+        #props["expiration_date_code"] = self._sanitize(data[0x834f : 0x834f + 17])
+        props["expiration_date"] = self._convertPvgDate(data[0x834f : 0x834f + 17])
+        #props["effective_date_code"] = self._sanitize(data[0x8360 : 0x8360 + 17])
+        props["effective_date"] = self._convertPvgDate(data[0x8360 : 0x8360 + 17])
 
         return props
+
+    def _convertPvgDate(self, date):
+        '''
+        PVG date format is Year from 0001 to 9999
+                           Month from 01 to 12
+                           Day from 01 to 31.
+                           Hour from 00 to 23.
+                           Minute from 00 to 59.
+                           Second from 00 to 59.
+                           Hundredths of a second from 00 to 99
+                           Time zone offset from GMT as integer
+        '''
+        if date == b"0000000000000000\x00":
+            return ""
+        else:
+            return "%s-%s-%s %s:%s:%s.%s +%02d:%02d" % (date[0:4].decode("ascii"), date[4:6].decode("ascii"), date[6:8].decode("ascii"), \
+                                                  date[8:10].decode("ascii"), date[10:12].decode("ascii"), \
+                                                  date[12:14].decode("ascii"), date[14:16].decode("ascii"), \
+                                                  (date[16] / 4), (date[16] % 4 * 15))
 
 RomInfoParser.registerParser(GenericDiscParser())
